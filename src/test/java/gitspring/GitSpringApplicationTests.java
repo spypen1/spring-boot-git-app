ddb225846f04e8b5c371f7eba612dbbe556fda89
Augusto Garibaldi <augustolg17@gmail.com>
2023-07-26 00:37:41 -0300

    Primer commit

---
+++ src/test/java/gitspring/GitSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package gitspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
